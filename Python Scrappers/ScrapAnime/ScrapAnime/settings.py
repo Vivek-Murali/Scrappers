6a364e748a1082d8539a5a0e053714ab272a9f93
Vivek Murali <vivekmurali00@outlook.com>
2023-02-03 23:21:01 +0530

    added rules

--- Python Scrappers/ScrapAnime/ScrapAnime/settings.py
+++ Python Scrappers/ScrapAnime/ScrapAnime/settings.py
@@ -27,7 +27,7 @@ SPLASH_URL = 'http://192.168.59.103:8050'
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 3
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
--- Python Scrappers/ScrapAnime/ScrapAnime/spiders/animePlanet_spider.py
+++ Python Scrappers/ScrapAnime/ScrapAnime/spiders/animePlanet_spider.py
@@ -1,32 +1,33 @@
 import scrapy
-from scrapy_splash import SplashRequest
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
 
-
-class AnimePlanetAnimeSpider(scrapy.Spider):
+class AnimePlanetAnimeSpider(CrawlSpider):
     name = "AnimePlanetAnime"
-    #start_urls = ['https://www.anime-planet.com/anime/all',]
+    allowed_domains = ["anime-planet.com"]
+    start_urls = ['https://www.anime-planet.com/anime/all']
     
-    script='''fucntion main(splash,args)
-                url=args.url
-                assert(splash:go(url))
-                return splash:html()
-                end'''
-                
-    def start_request(self):
-        yield SplashRequest(url="https://anime-stats.net/anime/all-anime", callback=self.parse, endpoint="execute", args={'lua_source':self.script,'wait': 0.5})
+    rules = (
+        Rule(LinkExtractor(restrict_xpaths="//ul[@class='cardDeck cardGrid']/li/a"),callback='parse',follow=True),
+        Rule(LinkExtractor(restrict_xpaths="//div[@class='pagination aligncenter']/ul/li[@class='next']/a"))
+    )
     
-
     def parse(self, response):
         yield {
-            "body":response.body
-        }
-        """ for quote in response.css('li.card'):
-            yield {
-                'title': quote.css('h3::text').get(),
-                'link': quote.css('a::attr(href)').get(),
+                'title': response.xpath("//h1[@itemprop='name']/text()").get(),
+                'url': response.url,
+                'animeType': response.xpath("//div[@class='pure-1 md-1-5']/span[@class='type']/text()").get(),
+                'studio': response.xpath("//section[@class='pure-g entryBar']/div[2]/a/@href").getall(),
+                'year': response.xpath("//div[@class='pure-1 md-1-5']/span[@class='iconYear']/text()").get(),
+                "rating":response.xpath("//div[@class='pure-1 md-1-5']/div[@class='avgRating']/span/text()").get(),
+                "currentRank": response.xpath("//section[@class='pure-g entryBar']/div[5]/text()").get(),
+                "tags":response.xpath("//div[@class='pure-1 md-3-5']/div[@class='tags ']/ul/li/a/text()").getall(),
+                "waringNote":response.xpath("//div[@class='pure-1 md-3-5']/div[@class='tags tags--plain']/ul/li/a/text()").getall(),
+                "image":response.xpath("//div[@class='mainEntry']/img/@src").get(),
+                "recommandationurl":response.xpath("//div[@class='tabs reco-tabs ui-tabs ui-widget ui-widget-content ui-corner-all']/div/a/@href").get(),
+                "reviewurl":response.xpath("//section[2]/div[3]/a[@class='macro_button macro_button--secondary']/@href").get(),
+                "staffurl":response.xpath("//section[@class='EntryPage__content__section EntryPage__content__section__staff castaff']/a/@href").get(),
+                "charurl":response.xpath("//section[@class='EntryPage__content__section EntryPage__content__section__characters castaff']/a/@href").get(),
+                "connections":response.xpath("//div[@id='tabs--relations--anime']/div/div/a/@href").getall(),
+                "mangaconnections":response.xpath("//div[@id='tabs--relations--manga']/div/div/a/@href").getall()
             }
-
-        next_page = response.css('li.next a::attr(href)').get()
-        if next_page is not None:
-            next_page = response.urljoin(next_page)
-            yield scrapy.Request(next_page, callback=self.parse) """
